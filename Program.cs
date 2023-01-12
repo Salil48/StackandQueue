2bffec97c0c599c5a1b680ee83c1e02ddfd36f0e
“Salil48” <salilchharia@gmail.com>
2023-01-12 17:24:37 +0530

    Welcome to Stack and Queue Program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StackQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Program");
+            
+        }
+    }
+}
